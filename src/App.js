53d181c6a7485b4e20d5915833bf46c8e8d9ccc2
Russell <russellcape98@gmail.com>
2020-08-04 18:55:49 -0400

    Cleaned/Removed documents

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="Parent">
+      <header className="child-1">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Russell Cape
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
