62d5a568dc0d9537da0f69e50a0d283cfd0f5e72
Russell <russellcape98@gmail.com>
2020-08-05 22:31:52 -0400

    Organized Ideas and made general styling tags

--- src/App.js
+++ src/App.js
@@ -3,13 +3,28 @@ import './App.css';
 
 function App() {
   return (
-    <div className="Parent">
-      <header className="child-1">
-        <p>
-          Russell Cape
-        </p>
-        
+    <div className="App">
+      <header className="App-header">
+        <h1>This is the Header</h1>
       </header>
+      <body>
+        <p>
+          <lh>
+            <h2>Contents:</h2>
+          </lh>
+          <ul>
+            <li>
+              Profile Picture
+            </li>
+            <li>
+              Summary
+            </li>
+            <li>
+              Contact info
+            </li>
+          </ul>
+        </p>
+      </body>
     </div>
   );
 }
