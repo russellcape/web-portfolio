43501824694a434691c171a4b819e872819a9dda
Russell <russellcape98@gmail.com>
2020-08-12 14:55:59 -0400

    Adding background image

--- src/App.js
+++ src/App.js
@@ -1,27 +1,30 @@
 import React from 'react';
 import './App.css';
+import Background from './images/binary-wallpaper.jpeg'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-Header">
         <h1>This is the Header</h1>
       </header>
       <body>
-        <lh>
-          <h2>Contents:</h2>
-        </lh>
-        <ul>
-          <li>
-            Profile Picture
-          </li>
-          <li>
-            Summary
-          </li>
-          <li>
-            Contact info
-          </li>
-        </ul>
+        <div>
+          <lh>
+            <h2>Contents:</h2>
+          </lh>
+          <ul>
+            <li>
+              Profile Picture
+            </li>
+            <li>
+              Summary
+            </li>
+            <li>
+              Contact info
+            </li>
+          </ul>
+          </div>
       </body>
     </div>
   );
