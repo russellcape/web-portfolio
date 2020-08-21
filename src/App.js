971a3a78ef8b30c2dfe5a07df4376475c5b6f541
Russell <russellcape98@gmail.com>
2020-08-21 13:46:52 -0400

    Made the routes for each section

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import './App.css';
 import './images/binary-wallpaper.jpg'
+import { ProSidebar, Menu, MenuItem } from 'react-pro-sidebar';
+import 'react-pro-sidebar/dist/css/styles.css';
+import { 
+  BrowserRouter as Router,
+  Link, 
+  Route,
+  Switch
+} from 'react-router-dom'
 import Landing from './components/Landing';
 import Summary from './components/Summary';
 import Education from './components/Education';
@@ -11,25 +19,95 @@ import Awards from './components/Awards';
 import Activities from './components/Activities';
 import Volunteering from './components/Volunteering';
 import ContactInfo from './components/ContactInfo';
-import Nav from './components/Nav';
 
 function App() {
   return (
     <div className="App">
+      <Router>
+          <ProSidebar>
+            <Menu>
+              <MenuItem>
+                Landing
+                <Link to="/" />
+              </MenuItem>
+              <MenuItem>
+                Summary
+                <Link to="/summary" />
+              </MenuItem>
+              <MenuItem>
+                Education
+                <Link to="/education" />
+              </MenuItem>
+              <MenuItem >
+                Employment
+                <Link to="/employment" />
+              </MenuItem>
+              <MenuItem >
+                Projects
+                <Link to="/projects" />
+              </MenuItem>
+              <MenuItem >
+                Skills
+                <Link to="/skills" />
+              </MenuItem>
+              <MenuItem >
+                Awards
+                <Link to="/awards" />
+              </MenuItem>
+              <MenuItem >
+                Activities
+                <Link to="/activities" />
+              </MenuItem>
+              <MenuItem >
+                Volunteering
+                <Link to="/volunteering" />
+              </MenuItem>
+              <MenuItem >
+                Contact Info
+                <Link to="/contact" />
+              </MenuItem>
+            </Menu>
+          </ProSidebar>
+
+          {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+
+          <div className="routes">
+            <Switch>
+              <Route path="/contact">
+                <ContactInfo />
+              </Route>
+              <Route path="/volunteering">
+                <Volunteering />
+              </Route>
+              <Route path="/activities">
+                <Activities />
+              </Route>
+              <Route path="/awards">
+                <Awards />
+              </Route>
+              <Route path="/skills">
+                <Skills />
+              </Route>
+              <Route path="/projects">
+                <Projects />
+              </Route>
+              <Route path="/employment">
+                <Employment />
+              </Route>
+              <Route path="/education">
+                <Education />
+              </Route>
+              <Route path="/summary">
+                <Summary />
+              </Route>
+              <Route path="/">
+                <Landing />
+              </Route>
+            </Switch>
+          </div>
+        </Router>
       <body>
-        <Nav/>
-        <div className ="content">
-        <Landing/>
-        <Summary/>
-        <Education/>
-        <Employment/>
-        <Projects/>
-        <Skills/>
-        <Awards/>
-        <Activities/>
-        <Volunteering/>
-        <ContactInfo/>
-        </div>
       </body>
     </div>
   );
